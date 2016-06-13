a710702febc82c6b1f70a3b51eb58fe9f9919a1e
irma__000 <irma__000@IRMA>
2016-06-13 12:33:17 -0500

    Se agrego la capreta para anexar script para la base de datos

---
+++ Alegria-Embotellada/dbAlegriaEmbotelladaESC.sql
@@ -0,0 +1,10 @@
+﻿/**************************************************************************************************************************
+Nombre Script:    dbAlegriaEmbotelladaESC
+Description:   Script to creation of database
+version:    1.0 
+Historia de Modificación:   IS   -   Fecha     -  Descripcion Cambio
+                           ----      ---------    ------------------------------------------------------------- 
+						   JAC       13/06/2016   Creación del script de Base de Datos dbAlegriaEmbotelladaESC
+****************************************************************************************************************************/
+USE master
+GO
