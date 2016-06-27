2c8350f07051d10031a1bf4582ed368551b89c87
irma__000 <irma__000@IRMA>
2016-06-27 12:53:39 -0500

    Creación de las tablas del Codigo SQL

--- Alegria-Embotellada/dbAlegriaEmbotelladaESC.sql
+++ Alegria-Embotellada/dbAlegriaEmbotelladaESC.sql
@@ -73,20 +73,116 @@ BEGIN TRY
 	BEGIN TRAN
 		IF EXISTS(SELECT * FROM DBO.SYSDATABASES WHERE NAME = 'dbAlegriaEmbotelladaESC_UAT')
 			BEGIN		
-				IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES 
+			IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES 
 							WHERE TABLE_SCHEMA = 'dbo' 
-							AND  TABLE_NAME = 'Country'))
-					BEGIN
-						SELECT 'Table Country already exists' AS 'Msg'
+							AND  TABLE_NAME = 'tb_Login'))
+							BEGIN
+						SELECT 'Table Login already exists' AS 'Msg'
 					END
 					ELSE
 						BEGIN
-							CREATE TABLE Country
-							(
-
-							);
-						END
-						
+				CREATE TABLE tb_Login
+              (PK_Usuario INT not null PRIMARY KEY IDENTITY(1,1),
+               UserName VARCHAR (16)not null UNIQUE,
+               PASSWORD VARCHAR (16) not null,
+               FK_Consumidor INT not null )
+               END
+			   IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES 
+							WHERE TABLE_SCHEMA = 'dbo' 
+							AND  TABLE_NAME = 'tb_Consumidor'))
+							BEGIN
+						SELECT 'Table Consumidor already exists' AS 'Msg'
+					END
+					ELSE
+						BEGIN
+			    CREATE TABLE tb_Consumidor(
+               PK_Consumidor INT not null PRIMARY KEY IDENTITY(1,1),
+               Primer_Nombre VARCHAR(15)not null, 
+               Primer_Apellido VARCHAR(15) null,
+               edad INT not null,
+               email VARCHAR (30) not null UNIQUE, )
+			   END
+			    IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES 
+							WHERE TABLE_SCHEMA = 'dbo' 
+							AND  TABLE_NAME = 'tb_Sucursal'))
+							BEGIN
+						SELECT 'Table Sucursal already exists' AS 'Msg'
+					END
+					ELSE
+						BEGIN
+			   CREATE TABLE tb_Sucursal
+              (PK_Sucursal INT  not null,
+               Nombre_Sucursal VARCHAR(15) not null,
+               Calle VARCHAR(15) not null,
+               Número int not null,
+               Colonia VARCHAR(15) not null,
+               FK_Promoción int not null,
+                primary key (PK_Sucursal))
+				END
+				 IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES 
+							WHERE TABLE_SCHEMA = 'dbo' 
+							AND  TABLE_NAME = 'tb_Promoción'))
+							BEGIN
+						SELECT 'Table Promoción already exists' AS 'Msg'
+					END
+					ELSE
+						BEGIN
+				CREATE TABLE tb_Promoción
+               (PK_Promoción INT not null,
+               Dia_Inicio VARCHAR(10)not null,
+                Dia_Finalización VARCHAR (10) not null,
+                FK_Sucursal INT not null,
+                FK_Artículo INT not null,
+                Descripción VARCHAR (100) not null,
+                Cantidad_Artículo INT not null
+                PRIMARY KEY (PK_Promoción))
+				END
+				 IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES 
+							WHERE TABLE_SCHEMA = 'dbo' 
+							AND  TABLE_NAME = 'tb_Artículo'))
+							BEGIN
+						SELECT 'Table Artículo already exists' AS 'Msg'
+					END
+					ELSE
+						BEGIN
+				CREATE TABLE tb_Artículo
+               (PK_Artículo INT  not null,
+                Nombre VARCHAR (15) not null,
+                Marca VARCHAR (10) not null,
+                Precio INT not null,
+                Existencia VARCHAR (2) not null,
+                Presentacion INT not null,
+                FK_Proveedor INT not null
+                PRIMARY KEY (PK_Artículo))
+				END
+				 IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES 
+							WHERE TABLE_SCHEMA = 'dbo' 
+							AND  TABLE_NAME = 'tb_Proveedor'))
+							BEGIN
+						SELECT 'Table Proveedor already exists' AS 'Msg'
+					END
+					ELSE
+						BEGIN
+				 CREATE TABLE tb_Proveedor
+                (PK_Proveedor INT not null,
+                 Nombre VARCHAR (20) not null,
+                 Teléfono VARCHAR (20) null,
+                 email VARCHAR (50) null,
+                PRIMARY KEY (PK_Proveedor))
+                END
+                
+				 ALTER TABLE  tb_Promoción  ADD CONSTRAINT id_Promoción FOREIGN KEY (FK_Promoción)
+				 REFERENCES tb_Sucursal (PK_Promoción) 
+				 ALTER TABLE tb_Login   ADD CONSTRAINT cod_Consumidor FOREIGN KEY (FK_Consumidor)
+				 REFERENCES tb_Consumidor(PK_Consumidor)
+				 ALTER TABLE  tb_Promoción  ADD CONSTRAINT id_Promoción FOREIGN KEY (FK_Promoción)
+				 REFERENCES tb_Sucursal (PK_Promoción)
+				 ALTER TABLE  tb_Promoción  ADD CONSTRAINT cod_sucursal FOREIGN KEY (FK_Sucursal)
+				 REFERENCES tb_Sucursal (PK_Sucursal)
+				 ALTER TABLE  tb_Promoción  ADD CONSTRAINT cod_Articulo FOREIGN KEY (FK_Artículo)
+				 REFERENCES tb_Artículo (PK_Artículo)
+				 ALTER TABLE tb_Artículo   ADD CONSTRAINT id_Proveedor FOREIGN KEY (FK_Proveedor)
+				 REFERENCES tb_Proveedor (PK_Proveedor)
 				COMMIT	
 				
 			END
