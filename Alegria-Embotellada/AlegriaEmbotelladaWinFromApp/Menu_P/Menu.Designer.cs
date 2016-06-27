bc20b698d0b0524201ff4901679ad3f4987e2851
Juan Esteban LC <Juan Esteban LC@LENO197>
2016-06-26 21:32:35 -0500

    Interfaz(Menu) le agregamos (menuStrip, comboBox, checkedListBox, tabControl, pictureBox y groupBox)

--- Alegria-Embotellada/AlegriaEmbotelladaWinFromApp/Menu_P/Menu.Designer.cs
+++ Alegria-Embotellada/AlegriaEmbotelladaWinFromApp/Menu_P/Menu.Designer.cs
@@ -382,7 +382,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(7, 57);
+            this.comboBox1.Location = new System.Drawing.Point(7, 44);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 0;
