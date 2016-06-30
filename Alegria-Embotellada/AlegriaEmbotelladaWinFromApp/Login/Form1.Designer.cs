50a0cf2cdb849a8758fdcab66f9cc384912bd202
Juan Esteban LC <Juan Esteban LC@LENO197>
2016-06-30 02:43:40 -0500

    Modificamos el color de los label.

--- Alegria-Embotellada/AlegriaEmbotelladaWinFromApp/Login/Form1.Designer.cs
+++ Alegria-Embotellada/AlegriaEmbotelladaWinFromApp/Login/Form1.Designer.cs
@@ -117,7 +117,7 @@
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.linkLabel1.Location = new System.Drawing.Point(419, 369);
+            this.linkLabel1.Location = new System.Drawing.Point(420, 369);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(55, 13);
             this.linkLabel1.TabIndex = 4;
