75fa954d65b631b77ecee1cb0566634a95689149
irma__000 <irma__000@IRMA>
2016-07-13 11:03:59 -0500

    Creación de las Clases DAL

---
+++ Alegria-Embotellada/DataAcces/tb_ConsumidoDAL.cs
@@ -0,0 +1,24 @@
+﻿using DataAcces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces
+{
+    public class tb_ConsumidoDAL
+    {
+        #region Ingresar Datos Consumido
+        public static bool IngresarDatosConsumidor ( tb_Consumidor consumidor)
+        {
+            using (AlegriaEmbotelladaEntities bd = new AlegriaEmbotelladaEntities())
+            {
+                bd.tb_Consumidor.Add(consumidor);
+                bd.SaveChanges();
+                return true;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Alegria-Embotellada/DataAcces/tb_LoginDAL.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces
+{
+   public class tb_LoginDAL
+    {
+        #region Validar NameNick
+        public static bool validarNameNick (string username)
+        { using(AlegriaEmbotelladaEntities bd = new AlegriaEmbotelladaEntities())
+            {
+                var query = (from b in bd.tb_Login
+                             where b.UserName == username
+                             select b).Count();
+                if (query > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+            
+        }
+        #endregion
+        #region Validar Password
+        public static bool validarPassword(string password)
+        {
+            using(AlegriaEmbotelladaEntities bd = new AlegriaEmbotelladaEntities())
+            {
+                var query = (from b in bd.tb_Login
+                             where b.PASSWORD == password
+                             select b).Count();
+                if(query > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        #endregion
+        #region Insertar Datos 
+        public static bool IngresarDatos (tb_Login login)
+        {
+            using(AlegriaEmbotelladaEntities bd  = new AlegriaEmbotelladaEntities())
+            {
+                bd.tb_Login.Add(login);
+                bd.SaveChanges();
+                return true;
+            }
+        }
+        #endregion
+    }
+}
