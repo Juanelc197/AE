a807dd9ab382bbf5994f333eb494a3ee7dcdcead
Juan Esteban LC <Juan Esteban LC@LENO197>
2016-07-21 10:13:25 -0500

    Modificasion de la interfaz menu cambiando el texto de un label de lugar a sucursal

--- Alegria-Embotellada/AlegriaEmbotelladaWinFromApp/Menu_P/Menu.Designer.cs
+++ Alegria-Embotellada/AlegriaEmbotelladaWinFromApp/Menu_P/Menu.Designer.cs
@@ -85,9 +85,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(7, 25);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Lugar";
+            this.label2.Text = "Sucursal ";
             // 
             // dataGridView1
             // 
